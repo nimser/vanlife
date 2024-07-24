ea9e6d9975010eafafcb89f518994e9c38211990
Nicolas Wormser <nicolas@codewithflair.org>
2024-07-24 14:34:37 +0200

    Add production server config

--- server/app/config.js
+++ server/app/config.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 
 const app = express();
@@ -22,6 +23,26 @@ app.use(express.json());
 const apiRouter = require("./routers/api/router");
 
 app.use(apiRouter);
+
+const reactBuildPath = path.join(__dirname, "/../../client/dist");
+const publicFolderPath = path.join(__dirname, "/../public");
+
+// Serve react resources
+
+app.use(express.static(reactBuildPath));
+
+// Serve server resources
+
+app.use(
+  "/assets",
+  express.static(path.join(publicFolderPath, "assets"), { maxAge: "1y" })
+);
+
+// Redirect unhandled requests to the react index file
+
+app.get("*", (_, res) => {
+  res.sendFile(path.join(reactBuildPath, "index.html"));
+});
 
 // Important: Error-handling middleware should be defined last, after other app.use() and routes calls.
 
